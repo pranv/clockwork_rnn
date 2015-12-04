a59d749975c7423ad13db6686637b9ef3cae2dc8
Pranav Shyam <pranavshyam13@gmail.com>
2015-12-04 21:04:49 +0530

    added crnn_heir to init

--- layers/__init__.py
+++ layers/__init__.py
@@ -2,5 +2,6 @@ from activations import *
 from lstm import LSTM
 from crnn import CRNN
 from crnn_i import CRNN_I
+from crnn_heir import CRNN_HEIR
 from linear import Linear
 from softmax import Softmax
