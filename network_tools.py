bd2924406fe599ef0015c34df80066d93f53e5e2
Pranav Shyam <pranavshyam13@gmail.com>
2015-12-15 15:02:22 +0530

    reorder, return grad of input

--- network_tools.py
+++ network_tools.py
@@ -1,37 +1,5 @@
 import numpy as np
 
-def extract_weights(model):
-	weights = []
-	for layer in model:
-		w = layer.get_weights()
-		if w is None:
-			continue
-		weights.append(w)
-	W = np.concatenate(weights)
-	return W
-
-
-def load_weights(model, W):
-	for layer in model:
-		w = layer.get_weights()
-		if w is None:
-			continue
-		w_shape = w.shape
-		w, W = np.split(W, [np.prod(w_shape)])
-		layer.set_weights(w.reshape(w_shape))
-	return model
-
-
-def extract_grads(model):
-	grads = []
-	for layer in model:
-		g = layer.get_grads()
-		if g is None:
-			continue
-		grads.append(g)
-	dW = np.concatenate(grads)
-	return dW
-
 
 def forward(model, input):
 	for layer in model:
@@ -42,6 +10,39 @@ def forward(model, input):
 def backward(model, gradient):
 	for layer in reversed(model):
 		gradient = layer.backward(gradient)
+	return gradient
+
+
+def load_weights(model, W):
+	for layer in model:
+		w = layer.get_weights()
+		if w is None:
+			continue
+		w_shape = w.shape
+		w, W = np.split(W, [np.prod(w_shape)])
+		layer.set_weights(w.reshape(w_shape))
+
+
+def extract_weights(model):
+	weights = []
+	for layer in model:
+		w = layer.get_weights()
+		if w is None:
+			continue
+		weights.append(w)
+	W = np.concatenate(weights)
+	return np.array(W)
+
+
+def extract_grads(model):
+	grads = []
+	for layer in model:
+		g = layer.get_grads()
+		if g is None:
+			continue
+		grads.append(g)
+	dW = np.concatenate(grads)
+	return np.array(dW)
 
 
 def forget(model):
